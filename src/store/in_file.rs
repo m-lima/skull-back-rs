3a39ab543fd430a04a15534f660444afa9ec51c3
Marcelo Lima <m-lima@users.noreply.github.com>
2022-07-25 20:45:28 +0200

    WIP: Impl constraints in_file

--- src/store/in_file.rs
+++ src/store/in_file.rs
@@ -117,27 +117,27 @@ impl InFile {
 
 impl Store for InFile {
     fn skull(&self, user: &str) -> Result<&dyn Crud<Skull>, Error> {
-        let user = self
+        let user_file = self
             .users
             .get(user)
             .ok_or_else(|| Error::NoSuchUser(String::from(user)))?;
-        Ok(&user.skull)
+        Ok(user_file)
     }
 
     fn quick(&self, user: &str) -> Result<&dyn Crud<Quick>, Error> {
-        let user = self
+        let user_file = self
             .users
             .get(user)
             .ok_or_else(|| Error::NoSuchUser(String::from(user)))?;
-        Ok(&user.quick)
+        Ok(user_file)
     }
 
     fn occurrence(&self, user: &str) -> Result<&dyn Crud<Occurrence>, Error> {
-        let user = self
+        let user_file = self
             .users
             .get(user)
             .ok_or_else(|| Error::NoSuchUser(String::from(user)))?;
-        Ok(&user.occurrence)
+        Ok(user_file)
     }
 }
 
@@ -147,8 +147,36 @@ struct UserStore {
     occurrence: std::sync::RwLock<UserFile<Occurrence>>,
 }
 
+#[async_trait::async_trait]
+impl<D: FileData> Crud<D> for UserStore {
+    async fn list(&self, limit: Option<u32>) -> Response<Vec<D::Id>> {
+        D::list(self, limit)
+    }
+
+    async fn create(&self, data: D) -> Response<Id> {
+        D::create(self, data)
+    }
+
+    async fn read(&self, id: Id) -> Response<D::Id> {
+        D::read(self, id)
+    }
+
+    async fn update(&self, id: Id, data: D) -> Response<D::Id> {
+        D::update(self, id, data)
+    }
+
+    async fn delete(&self, id: Id) -> Response<D::Id> {
+        D::delete(self, id)
+    }
+
+    async fn last_modified(&self) -> Result<std::time::SystemTime, Error> {
+        D::last_modified(self)
+    }
+}
+
 struct UserFile<D: Data> {
     file: std::path::PathBuf,
+    next_id: u32,
     _marker: std::marker::PhantomData<D>,
 }
 
@@ -156,17 +184,48 @@ impl<D: FileData> UserFile<D> {
     fn new(file: std::path::PathBuf) -> Self {
         Self {
             file,
+            next_id: 1,
             _marker: std::marker::PhantomData,
         }
     }
 
-    fn lines(&self) -> Result<impl Iterator<Item = std::io::Result<String>>, Error> {
+    fn lines(&self) -> Result<impl Iterator<Item = D::Id> + '_, Error> {
         use std::io::BufRead;
-        let iter = std::io::BufReader::new(std::fs::File::open(&self.file)?).lines();
+        let iter = std::io::BufReader::new(std::fs::File::open(&self.file)?)
+            .lines()
+            .map(D::read_tsv)
+            .enumerate()
+            .filter_map(|line| match line.1 {
+                Ok(line) => Some(line),
+                Err(err) => {
+                    log::error!("Failed to read {}:{}: {err}", self.file.display(), line.0);
+                    None
+                }
+            });
         Ok(iter)
     }
 
-    fn replace(&mut self, entries: Vec<D::Id>) -> Result<(), Error> {
+    fn ids(&self) -> Result<impl Iterator<Item = Result<Id, Error>>, Error> {
+        use std::io::BufRead;
+        let iter = std::io::BufReader::new(std::fs::File::open(&self.file)?)
+            .lines()
+            .map(D::id);
+        Ok(iter)
+    }
+
+    fn append(&mut self, data: D) -> Result<(Id, std::time::SystemTime), Error> {
+        if self.next_id == u32::MAX {
+            return Err(Error::StoreFull);
+        }
+        let mut file = std::fs::File::options().append(true).open(&self.file)?;
+        let id = self.next_id;
+        D::write_tsv(D::Id::new(id, data), &mut file)?;
+        self.next_id += 1;
+
+        Ok((id, self.last_modified()?))
+    }
+
+    fn replace(&mut self, entries: Vec<D::Id>) -> Result<std::time::SystemTime, Error> {
         use std::io::Write;
 
         let mut buffer = vec![];
@@ -178,151 +237,238 @@ impl<D: FileData> UserFile<D> {
             .truncate(true)
             .write(true)
             .open(&self.file)?
-            .write_all(buffer.as_slice())
-            .map_err(Error::Io)
+            .write_all(buffer.as_slice())?;
+
+        self.last_modified()
     }
 
-    fn good_line<T>(&self, line: (usize, Result<T, Error>)) -> Option<T> {
-        match line.1 {
-            Ok(line) => Some(line),
-            Err(err) => {
-                log::error!("Failed to read {}:{}: {err}", self.file.display(), line.0);
-                None
-            }
-        }
-    }
-
-    fn good_line_with_index(
-        &self,
-        line: (usize, Result<D::Id, Error>),
-        id: Id,
-        index: &mut Option<usize>,
-    ) -> Option<D::Id> {
-        match line.1 {
-            Ok(entry) => {
-                if entry.id() == id {
-                    *index = Some(line.0);
-                }
-                Some(entry)
-            }
-            Err(err) => {
-                log::error!("Failed to read {}:{}: {err}", self.file.display(), line.0);
-                None
-            }
-        }
-    }
-}
-
-#[async_trait::async_trait]
-impl<D: FileData> Crud<D> for std::sync::RwLock<UserFile<D>> {
-    async fn list(&self, limit: Option<u32>) -> Response<Vec<D::Id>> {
-        let lock = self.read()?;
-
-        let entries = lock
-            .lines()?
-            .map(D::read_tsv)
-            .enumerate()
-            .filter_map(|line| lock.good_line(line))
-            .collect::<Vec<_>>();
-
-        if let Some(limit) = limit.map(usize::try_from).and_then(Result::ok) {
-            let len = entries.len();
-            Ok((
-                entries.into_iter().skip(len - limit).collect(),
-                last_modified(&lock.file)?,
-            ))
-        } else {
-            Ok((entries, last_modified(&lock.file)?))
-        }
-    }
-
-    async fn create(&self, data: D) -> Response<Id> {
-        let lock = self.write()?;
-        let id = lock
-            .lines()?
-            .map(D::id)
-            .enumerate()
-            .filter_map(|line| lock.good_line(line))
-            .max()
-            .map_or(1, |id| id + 1);
-
-        let mut file = std::fs::File::options().append(true).open(&lock.file)?;
-        D::write_tsv(D::Id::new(id, data), &mut file)?;
-
-        Ok((id, last_modified(&lock.file)?))
-    }
-
-    async fn read(&self, id: Id) -> Response<D::Id> {
-        let lock = self.read()?;
-        let data = lock
-            .lines()?
-            .map(D::read_tsv)
-            .enumerate()
-            .filter_map(|line| lock.good_line(line))
-            .find(|d| d.id() == id)
-            .ok_or(Error::NotFound(id))?;
-        Ok((data, last_modified(&lock.file)?))
-    }
-
-    async fn update(&self, id: Id, data: D) -> Response<D::Id> {
-        let mut lock = self.write()?;
-        let mut index = None;
-        let mut entries = lock
-            .lines()?
-            .map(D::read_tsv)
-            .enumerate()
-            .filter_map(|line| lock.good_line_with_index(line, id, &mut index))
-            .collect::<Vec<_>>();
-
-        let index = index.ok_or(Error::NotFound(id))?;
-
-        let old = &mut entries[index];
-        let mut new = D::Id::new(id, data);
-        std::mem::swap(old, &mut new);
-
-        lock.replace(entries)?;
-        Ok((new, last_modified(&lock.file)?))
-    }
-
-    async fn delete(&self, id: Id) -> Response<D::Id> {
-        let mut lock = self.write()?;
-        let mut index = None;
-        let mut entries = lock
-            .lines()?
-            .map(D::read_tsv)
-            .enumerate()
-            .filter_map(|line| lock.good_line_with_index(line, id, &mut index))
-            .collect::<Vec<_>>();
-
-        let index = index.ok_or(Error::NotFound(id))?;
-        let old = entries.remove(index);
-
-        lock.replace(entries)?;
-        Ok((old, last_modified(&lock.file)?))
-    }
-
-    async fn last_modified(&self) -> Result<std::time::SystemTime, Error> {
-        let lock = self.read()?;
-        std::fs::metadata(&lock.file)
+    fn last_modified(&self) -> Result<std::time::SystemTime, Error> {
+        std::fs::metadata(&self.file)
             .and_then(|f| f.modified())
             .map_err(Error::Io)
     }
 }
 
-fn last_modified(path: &std::path::Path) -> Result<std::time::SystemTime, Error> {
-    std::fs::metadata(path)
-        .and_then(|f| f.modified())
-        .map_err(Error::Io)
+trait FileData: Serializable + 'static {
+    fn get(store: &UserStore) -> &std::sync::RwLock<UserFile<Self>>;
+    fn create(store: &UserStore, data: Self) -> Response<Id>;
+    fn update(store: &UserStore, id: Id, data: Self) -> Response<Self::Id>;
+    fn delete(store: &UserStore, id: Id) -> Response<Self::Id>;
+    fn conflicts(&self, other: &Self::Id) -> bool;
+
+    fn list(store: &UserStore, limit: Option<u32>) -> Response<Vec<Self::Id>> {
+        let lock = Self::as_read(store)?;
+
+        let entries = lock.lines()?.collect::<Vec<_>>();
+
+        if let Some(limit) = limit.map(usize::try_from).and_then(Result::ok) {
+            let len = entries.len();
+            Ok((
+                entries.into_iter().skip(len - limit.min(len)).collect(),
+                lock.last_modified()?,
+            ))
+        } else {
+            Ok((entries, lock.last_modified()?))
+        }
+    }
+
+    fn read(store: &UserStore, id: Id) -> Response<Self::Id> {
+        let lock = Self::as_read(store)?;
+        let data = lock
+            .lines()?
+            .find(|d| d.id() == id)
+            .ok_or(Error::NotFound(id))?;
+        Ok((data, lock.last_modified()?))
+    }
+
+    fn last_modified(store: &UserStore) -> Result<std::time::SystemTime, Error> {
+        Self::as_read(store)?.last_modified()
+    }
+
+    fn update_inner(
+        store: &UserStore,
+        index: usize,
+        mut entries: Vec<Self::Id>,
+        mut data: Self::Id,
+    ) -> Response<Self::Id> {
+        let left = &mut entries[index];
+        let last_modified = if left == &data {
+            Self::as_read(store)?.last_modified()
+        } else {
+            std::mem::swap(left, &mut data);
+            Self::as_write(store)?.replace(entries)
+        }?;
+        Ok((data, last_modified))
+    }
+
+    fn delete_inner(store: &UserStore, id: Id) -> Response<Self::Id> {
+        let (index, mut entries) = Self::entries_with_index(store, id)?;
+
+        let old = entries.remove(index);
+
+        let last_modified = Self::as_write(store)?.replace(entries)?;
+        Ok((old, last_modified))
+    }
+
+    fn has_skull(store: &UserStore, skull: Id) -> Result<(), Error> {
+        Skull::as_read(store)?
+            .ids()?
+            .filter_map(Result::ok)
+            .find(|id| *id == skull)
+            .map(|_| ())
+            .ok_or(Error::Constraint)
+    }
+
+    fn entries_with_index(store: &UserStore, id: Id) -> Result<(usize, Vec<Self::Id>), Error> {
+        let mut index = None;
+        let entries = Self::as_read(store)?
+            .lines()?
+            .enumerate()
+            .map(|line| {
+                if line.1.id() == id {
+                    index = Some(line.0);
+                }
+                line.1
+            })
+            .collect::<Vec<_>>();
+        index.map(|i| (i, entries)).ok_or(Error::NotFound(id))
+    }
+
+    fn as_read(store: &UserStore) -> Result<std::sync::RwLockReadGuard<'_, UserFile<Self>>, Error> {
+        Ok(Self::get(store).read()?)
+    }
+
+    fn as_write(
+        store: &UserStore,
+    ) -> Result<std::sync::RwLockWriteGuard<'_, UserFile<Self>>, Error> {
+        Ok(Self::get(store).write()?)
+    }
 }
 
-pub trait FileData: super::Data {
+impl FileData for Skull {
+    fn get(store: &UserStore) -> &std::sync::RwLock<UserFile<Self>> {
+        &store.skull
+    }
+
+    fn create(store: &UserStore, data: Self) -> Response<Id> {
+        if Self::as_read(store)?.lines()?.any(|d| data.conflicts(&d)) {
+            Err(Error::Constraint)
+        } else {
+            Self::as_write(store)?.append(data)
+        }
+    }
+
+    fn update(store: &UserStore, id: Id, data: Self) -> Response<Self::Id> {
+        let (index, entries) = Self::entries_with_index(store, id)?;
+
+        if entries
+            .iter()
+            .filter(|d| d.id != id)
+            .any(|d| data.conflicts(d))
+        {
+            Err(Error::Constraint)
+        } else {
+            Self::update_inner(store, index, entries, Self::Id::new(id, data))
+        }
+    }
+
+    fn delete(store: &UserStore, id: Id) -> Response<Self::Id> {
+        if Occurrence::as_read(store)?.lines()?.any(|d| d.skull == id) {
+            Err(Error::Constraint)
+        } else {
+            let quicks = Quick::as_read(store)?
+                .lines()?
+                .filter(|d| d.skull != id)
+                .collect();
+
+            let mut quick_lock = Quick::as_write(store)?;
+            let (old, last_modified) = {
+                let (index, mut entries) = Self::entries_with_index(store, id)?;
+                let old = entries.remove(index);
+                let last_modified = Self::as_write(store)?.replace(entries)?;
+                (old, last_modified)
+            };
+            quick_lock.replace(quicks)?;
+
+            Ok((old, last_modified))
+        }
+    }
+
+    fn conflicts(&self, other: &Self::Id) -> bool {
+        self.name == other.name || self.color == other.color || self.icon == other.icon
+    }
+}
+
+impl FileData for Quick {
+    fn get(store: &UserStore) -> &std::sync::RwLock<UserFile<Self>> {
+        &store.quick
+    }
+
+    fn create(store: &UserStore, data: Self) -> Response<Id> {
+        Self::has_skull(store, data.skull)?;
+        if Self::as_read(store)?.lines()?.any(|d| data.conflicts(&d)) {
+            Err(Error::Constraint)
+        } else {
+            Self::as_write(store)?.append(data)
+        }
+    }
+
+    fn update(store: &UserStore, id: Id, data: Self) -> Response<Self::Id> {
+        let (index, entries) = Self::entries_with_index(store, id)?;
+        Self::has_skull(store, data.skull)?;
+        if entries
+            .iter()
+            .filter(|d| d.id != id)
+            .any(|d| data.conflicts(d))
+        {
+            Err(Error::Constraint)
+        } else {
+            Self::update_inner(store, index, entries, Self::Id::new(id, data))
+        }
+    }
+
+    fn delete(store: &UserStore, id: Id) -> Response<Self::Id> {
+        Self::delete_inner(store, id)
+    }
+
+    fn conflicts(&self, other: &Self::Id) -> bool {
+        self.skull == other.skull && (self.amount - other.amount).abs() < f32::EPSILON
+    }
+}
+
+impl FileData for Occurrence {
+    fn get(store: &UserStore) -> &std::sync::RwLock<UserFile<Self>> {
+        &store.occurrence
+    }
+
+    fn create(store: &UserStore, data: Self) -> Response<Id> {
+        Self::has_skull(store, data.skull)?;
+        Self::as_write(store)?.append(data)
+    }
+
+    fn update(store: &UserStore, id: Id, data: Self) -> Response<Self::Id> {
+        let (index, entries) = Self::entries_with_index(store, id)?;
+        Self::has_skull(store, data.skull)?;
+        Self::update_inner(store, index, entries, Self::Id::new(id, data))
+    }
+
+    fn delete(store: &UserStore, id: Id) -> Response<Self::Id> {
+        Self::delete_inner(store, id)
+    }
+
+    fn conflicts(&self, _other: &Self::Id) -> bool {
+        false
+    }
+}
+
+trait Serializable: Data {
     fn name() -> &'static str;
     fn id(string: std::io::Result<String>) -> Result<Id, Error>;
     fn read_tsv(string: std::io::Result<String>) -> Result<Self::Id, Error>;
     fn write_tsv<W: std::io::Write>(with_id: Self::Id, writer: &mut W) -> Result<(), Error>;
 }
 
-impl FileData for Skull {
+impl Serializable for Skull {
     fn name() -> &'static str {
         "skull"
     }
@@ -383,7 +529,7 @@ impl FileData for Skull {
     }
 }
 
-impl FileData for Quick {
+impl Serializable for Quick {
     fn name() -> &'static str {
         "quick"
     }
@@ -415,7 +561,7 @@ impl FileData for Quick {
     }
 }
 
-impl FileData for Occurrence {
+impl Serializable for Occurrence {
     fn name() -> &'static str {
         "occurrence"
     }
@@ -466,7 +612,7 @@ mod test {
         test_util::{create_base_test_path, TestPath},
     };
 
-    use super::{Error, FileData, InFile, Skull, Store};
+    use super::{Data, Error, FileData, InFile, Serializable, Skull, Store, UserFile, UserStore};
 
     crate::impl_crud_tests!(InFile, TestStore::new());
 
@@ -504,6 +650,36 @@ mod test {
         ) -> Result<&dyn crate::store::Crud<crate::store::Occurrence>, Error> {
             self.store.occurrence(user)
         }
+    }
+
+    #[test]
+    fn create_store_full() {
+        fn full_container<D: Data>() -> std::sync::RwLock<UserFile<D>> {
+            std::sync::RwLock::new(UserFile {
+                next_id: u32::MAX,
+                file: std::path::PathBuf::new(),
+                _marker: std::marker::PhantomData,
+            })
+        }
+
+        let store = UserStore {
+            skull: full_container(),
+            quick: full_container(),
+            occurrence: full_container(),
+        };
+
+        let skull = Skull {
+            name: String::from("skull"),
+            color: String::from("red"),
+            icon: String::new(),
+            unit_price: 1.,
+            limit: None,
+        };
+
+        assert_eq!(
+            Skull::create(&store, skull).unwrap_err().to_string(),
+            Error::StoreFull.to_string()
+        );
     }
 
     #[test]
