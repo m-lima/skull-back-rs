e01fde7e75b897eb0c4f432562a332406580f33d
Marcelo Lima <m-lima@users.noreply.github.com>
2022-07-25 13:34:24 +0200

    WIP: Clean-up unit tests
    Missing:
    - Match memory and file with DB behavior (constraints mostly)

--- src/store/in_memory.rs
+++ src/store/in_memory.rs
@@ -200,18 +200,11 @@ impl MemoryData for Occurrence {
 
 #[cfg(test)]
 mod test {
-    use crate::{
-        check,
-        store::{
-            test::USER,
-            test_util::{last_modified_eq, last_modified_ne},
-            Quick, Selector,
-        },
-    };
+    use crate::store::test::USER;
 
     use super::{Crud, Error, InMemory, Skull, UserContainer};
 
-    crate::create_tests!(InMemory, InMemory::new(&[USER]));
+    crate::impl_crud_tests!(InMemory, InMemory::new(&[USER]));
 
     mod construction {
         use super::InMemory;
@@ -256,93 +249,6 @@ mod test {
         }
     }
 
-    fn new_skull(name: &str, unit_price: f32) -> Skull {
-        Skull {
-            name: String::from(name),
-            color: String::from("red"),
-            icon: String::new(),
-            unit_price,
-            limit: None,
-        }
-    }
-
-    impl InMemory {
-        fn test(&self) -> &std::sync::RwLock<UserContainer<Skull>> {
-            self.skull.data.get(USER).unwrap()
-        }
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn last_modified() {
-        let mut store = InMemory::new(&[USER]);
-
-        let last_modified = store.test().last_modified().await.unwrap();
-
-        // List [no change]
-        let op_time = store.test().list(None).await.unwrap().1;
-        let last_modified = check!(last_modified_eq(store.test(), last_modified, op_time).await);
-
-        // Create [change]
-        let op_time = store.test().create(new_skull("bla", 1.0)).await.unwrap().1;
-        let last_modified = check!(last_modified_ne(store.test(), last_modified, op_time).await);
-
-        // Read [no change]
-        let op_time = Crud::read(store.test(), 1).await.unwrap().1;
-        let last_modified = check!(last_modified_eq(store.test(), last_modified, op_time).await);
-
-        // Update [change]
-        let op_time = store
-            .test()
-            .update(1, new_skull("bla", 2.0))
-            .await
-            .unwrap()
-            .1;
-        let last_modified = check!(last_modified_ne(store.test(), last_modified, op_time).await);
-
-        // Delete [change]
-        let op_time = store.test().delete(1).await.unwrap().1;
-        let last_modified = check!(last_modified_ne(store.test(), last_modified, op_time).await);
-
-        // Create failure [no change]
-        store
-            .skull
-            .data
-            .get_mut(USER)
-            .unwrap()
-            .write()
-            .unwrap()
-            .next_id = u32::MAX;
-        assert!(store.test().create(new_skull("bla", 1.0)).await.is_err());
-        let last_modified = check!(last_modified_eq(store.test(), last_modified, None).await);
-
-        // Update failure [no change]
-        assert!(store.test().update(3, new_skull("bla", 1.0)).await.is_err());
-        let last_modified = check!(last_modified_eq(store.test(), last_modified, None).await);
-
-        // Delete failure [no change]
-        assert!(store.test().delete(5).await.is_err());
-        let last_modified = check!(last_modified_eq(store.test(), last_modified, None).await);
-
-        // Stores don't affect each other
-        Quick::select(&store, USER)
-            .unwrap()
-            .create(Quick {
-                skull: 0,
-                amount: 3.0,
-            })
-            .await
-            .unwrap();
-        let last_modified = check!(last_modified_eq(store.test(), last_modified, None).await);
-        assert_ne!(
-            Quick::select(&store, USER)
-                .unwrap()
-                .last_modified()
-                .await
-                .unwrap(),
-            last_modified
-        );
-    }
-
     #[tokio::test(flavor = "multi_thread")]
     async fn create_store_full() {
         let container = std::sync::RwLock::new(UserContainer {
@@ -350,7 +256,14 @@ mod test {
             data: Vec::new(),
             last_modified: std::time::SystemTime::now(),
         });
-        let skull = new_skull("skull", 0.4);
+
+        let skull = Skull {
+            name: String::from("skull"),
+            color: String::from("red"),
+            icon: String::new(),
+            unit_price: 1.,
+            limit: None,
+        };
 
         assert_eq!(
             container.create(skull).await.unwrap_err().to_string(),
