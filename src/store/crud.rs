7daec5cf87db9981816560087fb6e1db6f382324
Marcelo Lima <m-lima@users.noreply.github.com>
2023-01-28 18:46:15 +0100

    Remove most of the async_trait

--- src/store/crud.rs
+++ src/store/crud.rs
@@ -1,4 +1,7 @@
-use super::{Data, Error, Id, Occurrence, Quick, Skull};
+use super::{
+    in_db::SqlData, in_file::FileData, in_memory::MemoryData, Data, Error, Id, Occurrence, Quick,
+    Skull,
+};
 
 pub trait Store: Send + Sync + std::panic::RefUnwindSafe + 'static {
     type Crud<D: Selector>: Crud<D>;
@@ -10,19 +13,7 @@ pub trait Store: Send + Sync + std::panic::RefUnwindSafe + 'static {
 
 pub type Response<T> = Result<(T, std::time::SystemTime), Error>;
 
-#[async_trait::async_trait]
-pub trait Crud<D: Data>: Send + Sync {
-    async fn list(&self, limit: Option<u32>) -> Response<Vec<D::Id>>;
-    async fn create(&self, data: D) -> Response<Id>;
-    async fn read(&self, id: Id) -> Response<D::Id>;
-    async fn update(&self, id: Id, data: D) -> Response<D::Id>;
-    async fn delete(&self, id: Id) -> Response<D::Id>;
-    async fn last_modified(&self) -> Result<std::time::SystemTime, Error>;
-}
-
-pub trait Selector:
-    Data + super::in_db::SqlData + super::in_file::FileData + super::in_memory::MemoryData
-{
+pub trait Selector: Data + SqlData + FileData + MemoryData {
     fn select<'a, S: Store>(store: &'a S, user: &str) -> Result<&'a S::Crud<Self>, Error>;
 }
 
@@ -39,3 +30,36 @@ macro_rules! impl_selector {
 impl_selector!(Skull, skull);
 impl_selector!(Quick, quick);
 impl_selector!(Occurrence, occurrence);
+
+pub struct SyncResponse<T>(Option<T>);
+
+impl<T> SyncResponse<T> {
+    pub fn new(data: T) -> Self {
+        Self(Some(data))
+    }
+}
+
+impl<T: Unpin + Send> std::future::Future for SyncResponse<T> {
+    type Output = T;
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        _ctx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        match std::pin::Pin::into_inner(self).0.take() {
+            Some(data) => std::task::Poll::Ready(data),
+            None => panic!("Double polled a ready Future"),
+        }
+    }
+}
+
+pub trait Crud<D: Data>: Send + Sync {
+    type Future<T: Send + Unpin>: std::future::Future<Output = T> + Send;
+
+    fn list(&self, limit: Option<u32>) -> Self::Future<Response<Vec<D::Id>>>;
+    fn create(&self, data: D) -> Self::Future<Response<Id>>;
+    fn read(&self, id: Id) -> Self::Future<Response<D::Id>>;
+    fn update(&self, id: Id, data: D) -> Self::Future<Response<D::Id>>;
+    fn delete(&self, id: Id) -> Self::Future<Response<D::Id>>;
+    fn last_modified(&self) -> Self::Future<Result<std::time::SystemTime, Error>>;
+}
