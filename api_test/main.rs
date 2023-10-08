b0c7f32e61b209ec714ea44a01a88d802cff66cc
Marcelo Lima <m-lima@users.noreply.github.com>
2023-10-08 11:46:50 +0200

    Testing API, but multiple times for no reason

--- api_test/main.rs
+++ api_test/main.rs
@@ -1,33 +1,69 @@
 mod client;
 mod helper;
-mod in_memory;
 mod server;
+mod tests;
 
-fn block_on<F: std::future::Future>(future: F) -> F::Output {
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("Failed building the Runtime")
-        .block_on(future)
-}
-
-fn main() {
+fn main() -> std::process::ExitCode {
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .expect("Failed building the Runtime");
 
-    let server = runtime.block_on(server::start());
+    let errors = run(&runtime, server::Mode::Memory);
+    println!();
+    if !errors.is_empty() {
+        println!("failures:");
+        for error in errors {
+            println!("    {error}");
+        }
+        println!();
+        return std::process::ExitCode::FAILURE;
+    }
 
-    match std::thread::scope(|s| {
-        s.spawn(|| {
-            // std::panic::catch_unwind(|| {
-            runtime.block_on(in_memory::list(client::Client::from(&server)))
-        })
-    })
-    .join()
-    {
-        Ok(()) => println!("Ok"),
-        Err(err) => eprintln!("{err:?}"),
+    let errors = run(&runtime, server::Mode::File(test_utils::TestPath::new()));
+    println!();
+    if !errors.is_empty() {
+        println!("failures:");
+        for error in errors {
+            println!("    {error}");
+        }
+        println!();
+        return std::process::ExitCode::FAILURE;
+    }
+
+    let errors = run(&runtime, server::Mode::Db(test_utils::TestPath::new()));
+    println!();
+    if !errors.is_empty() {
+        println!("failures:");
+        for error in errors {
+            println!("    {error}");
+        }
+        println!();
+        return std::process::ExitCode::FAILURE;
+    }
+
+    std::process::ExitCode::SUCCESS
+}
+
+fn run(runtime: &tokio::runtime::Runtime, mode: server::Mode) -> Vec<String> {
+    let server = runtime.block_on(server::start(mode));
+
+    let tests = tests::test(runtime, &server);
+    if tests.is_empty() {
+        Vec::new()
+    } else {
+        println!("running {} tests", tests.len());
+        tests
+            .into_iter()
+            .filter_map(|(name, test)| {
+                if test.is_ok() {
+                    println!("test {name} ... [32mok[m");
+                    None
+                } else {
+                    println!("test {name} ... [31mFAILED[m");
+                    Some(name)
+                }
+            })
+            .collect()
     }
 }
