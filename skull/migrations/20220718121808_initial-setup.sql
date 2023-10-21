bb1b2b90312f42e3b8c5c1ebf2783ac10947db1a
Marcelo Lima <m-lima@users.noreply.github.com>
2023-10-21 18:03:16 +0200

    Add all, why not?

--- skull/migrations/20220718121808_initial-setup.sql
+++ skull/migrations/20220718121808_initial-setup.sql
@@ -1,30 +1,30 @@
 -- Skulls
-CREATE TABLE "skulls" (
+CREATE TABLE skulls (
   "id"         INTEGER NOT NULL PRIMARY KEY,
-  "name"       TEXT    NOT NULL UNIQUE,
-  "color"      TEXT    NOT NULL UNIQUE,
-  "icon"       TEXT    NOT NULL UNIQUE,
-  "unit_price" REAL    NOT NULL,
+  "name"       TEXT    NOT NULL UNIQUE CHECK(LENGTH(TRIM(name)) > 0),
+  "color"      TEXT    NOT NULL UNIQUE CHECK(LENGTH(TRIM(color)) > 0),
+  "icon"       TEXT    NOT NULL UNIQUE CHECK(LENGTH(TRIM(icon)) > 0),
+  "unit_price" REAL    NOT NULL        CHECK(unit_price >= 0),
   "limit"      REAL
 );
 
 -- Quicks
-CREATE TABLE "quicks" (
+CREATE TABLE quicks (
   "id"     INTEGER NOT NULL PRIMARY KEY,
   "skull"  INTEGER NOT NULL,
   "amount" REAL    NOT NULL,
 
-  FOREIGN KEY("skull") REFERENCES "skulls"("id") ON DELETE CASCADE,
+  FOREIGN KEY(skull) REFERENCES skulls(id) ON DELETE CASCADE,
 
-  UNIQUE("skull", "amount")
+  UNIQUE(skull, amount)
 );
 
 -- Occurrences
-CREATE TABLE "occurrences" (
+CREATE TABLE occurrences (
   "id"     INTEGER NOT NULL PRIMARY KEY,
   "skull"  INTEGER NOT NULL,
   "amount" REAL    NOT NULL,
   "millis" INTEGER NOT NULL,
 
-  FOREIGN KEY("skull") REFERENCES "skulls"("id") ON DELETE RESTRICT
+  FOREIGN KEY(skull) REFERENCES skulls(id) ON DELETE RESTRICT
 );
--- skull/migrations/20220721114208_last-modified.sql
+++ skull/migrations/20220721114208_last-modified.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "last_modified" (
-  "table"  INTEGER NOT NULL PRIMARY KEY,
-  "millis" INTEGER NOT NULL
+CREATE TABLE last_modified (
+  table  INTEGER NOT NULL PRIMARY KEY,
+  millis INTEGER NOT NULL
 );
 
-INSERT INTO "last_modified" (
-  "table",
-  "millis"
+INSERT INTO last_modified (
+  table,
+  millis
 ) VALUES (
   0,
   strftime("%s", "now") || substr(strftime("%f", "now"), 4)
@@ -18,70 +18,70 @@ INSERT INTO "last_modified" (
 );
 
 -- Skulls [0]
-CREATE TRIGGER "skulls_last_modified_insert" AFTER INSERT ON skulls
+CREATE TRIGGER skulls_last_modified_insert AFTER INSERT ON skulls
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 0;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 0;
 END;
-CREATE TRIGGER "skulls_last_modified_update" AFTER UPDATE ON skulls
+CREATE TRIGGER skulls_last_modified_update AFTER UPDATE ON skulls
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 0;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 0;
 END;
-CREATE TRIGGER "skulls_last_modified_delete" AFTER DELETE ON skulls
+CREATE TRIGGER skulls_last_modified_delete AFTER DELETE ON skulls
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 0;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 0;
 END;
 
 -- Quicks [1]
-CREATE TRIGGER "quicks_last_modified_insert" AFTER INSERT ON quicks
+CREATE TRIGGER quicks_last_modified_insert AFTER INSERT ON quicks
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 1;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 1;
 END;
-CREATE TRIGGER "quicks_last_modified_update" AFTER UPDATE ON quicks
+CREATE TRIGGER quicks_last_modified_update AFTER UPDATE ON quicks
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 1;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 1;
 END;
-CREATE TRIGGER "quicks_last_modified_delete" AFTER DELETE ON quicks
+CREATE TRIGGER quicks_last_modified_delete AFTER DELETE ON quicks
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 1;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 1;
 END;
 
 -- Occurrences [2]
-CREATE TRIGGER "occurrences_last_modified_insert" AFTER INSERT ON occurrences
+CREATE TRIGGER occurrences_last_modified_insert AFTER INSERT ON occurrences
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 2;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 2;
 END;
-CREATE TRIGGER "occurrences_last_modified_update" AFTER UPDATE ON occurrences
+CREATE TRIGGER occurrences_last_modified_update AFTER UPDATE ON occurrences
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 2;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 2;
 END;
-CREATE TRIGGER "occurrences_last_modified_delete" AFTER DELETE ON occurrences
+CREATE TRIGGER occurrences_last_modified_delete AFTER DELETE ON occurrences
 BEGIN
-  UPDATE "last_modified"
+  UPDATE last_modified
   SET
-    "millis" = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
-  WHERE "table" = 2;
+    millis = strftime("%s", "now") || substr(strftime("%f", "now"), 4)
+  WHERE table = 2;
 END;
