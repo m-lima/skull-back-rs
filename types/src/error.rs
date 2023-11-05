80019ceb270b0c91b18ec0d0cf2c321747d6c41f
Marcelo Lima <m-lima@users.noreply.github.com>
2023-11-05 12:13:44 +0100

    Impl Display for Kind

--- types/src/error.rs
+++ types/src/error.rs
@@ -10,3 +10,13 @@ pub enum Kind {
     NotFound,
     InternalError,
 }
+
+impl std::fmt::Display for Kind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::BadRequest => f.write_str("Bad Request"),
+            Self::NotFound => f.write_str("Not Found"),
+            Self::InternalError => f.write_str("Internal Error"),
+        }
+    }
+}
