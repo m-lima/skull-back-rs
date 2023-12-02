644791d69048390f4560d6b4de97c718c193019a
Marcelo Lima <m-lima@users.noreply.github.com>
2023-12-02 14:59:41 +0100

    Simplify ws request type names

--- server/src/ws/mode.rs
+++ server/src/ws/mode.rs
@@ -7,7 +7,7 @@ pub trait Mode: sealed::Sealed + Sized + Send + 'static {
         response: types::Message,
     ) -> Result<axum::extract::ws::Message, Self::SerializeError>;
     fn try_extract_id(bytes: &[u8]) -> Option<u32>;
-    fn deserialize(bytes: &[u8]) -> Result<types::WithId<types::Request>, Self::DeserializeError>;
+    fn deserialize(bytes: &[u8]) -> Result<types::ws::Request, Self::DeserializeError>;
 }
 
 impl Mode for String {
@@ -25,10 +25,10 @@ impl Mode for String {
     }
 
     fn try_extract_id(bytes: &[u8]) -> Option<u32> {
-        serde_json::from_slice::<types::WithId<()>>(bytes).map_or(None, |r| r.id)
+        serde_json::from_slice::<types::ws::Id>(bytes).map_or(None, |r| r.id)
     }
 
-    fn deserialize(bytes: &[u8]) -> Result<types::WithId<types::Request>, Self::DeserializeError> {
+    fn deserialize(bytes: &[u8]) -> Result<types::ws::Request, Self::DeserializeError> {
         serde_json::from_slice(bytes)
     }
 }
@@ -48,10 +48,10 @@ impl Mode for Vec<u8> {
     }
 
     fn try_extract_id(bytes: &[u8]) -> Option<u32> {
-        rmp_serde::from_slice::<types::WithId<()>>(bytes).map_or(None, |r| r.id)
+        rmp_serde::from_slice::<types::ws::Id>(bytes).map_or(None, |r| r.id)
     }
 
-    fn deserialize(bytes: &[u8]) -> Result<types::WithId<types::Request>, Self::DeserializeError> {
+    fn deserialize(bytes: &[u8]) -> Result<types::ws::Request, Self::DeserializeError> {
         rmp_serde::from_slice(bytes)
     }
 }
