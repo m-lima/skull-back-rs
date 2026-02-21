f6c9546175d09a20d909da4e8d95efe9564ef480
Marcelo Lima <m-lima@users.noreply.github.com>
2026-02-21 17:59:59 +0100

    Nixify

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    helper.url = "github:m-lima/nix-template";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      helper,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        base = helper.lib.rust.helper inputs system ./. {
+          buildInputs = pkgs: [ pkgs.openssl ];
+          nativeBuildInputs = pkgs: [ pkgs.pkg-config ];
+        };
+      in
+      base.outputs
+    );
+}
